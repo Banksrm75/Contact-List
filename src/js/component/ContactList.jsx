45681d2d325fb60ef7bf2c0fe3d50f5dfd8280d9
Rebekah Banks <banksrm75@gmail.com>
2024-11-05 00:40:13 +0000

    Clone to VSCode

---
+++ src/js/component/ContactList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from "./Contact.jsx";
+
+const ContactList = () => {
+
+    return(
+        <>
+            <Contact />
+        </>
+    );
+}
+
+export default ContactList;
